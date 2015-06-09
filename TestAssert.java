50de0db7107d772489e2a124e8511cc7a68e42bd
epickard <epickard@users.noreply.github.com>
2015-06-08 17:03:20 -0700

    Additional work on learning JUnit Assert

--- TestAssert.java
+++ TestAssert.java
@@ -5,42 +5,110 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-//Checkpoint checkin; dev in progress
+//Assert Same/Not Same
 public class TestAssert {
-	//Create class variables
-	String id;
-	String id2;
-	Integer int1;
-	Integer int2;
-	Double num1;
-	Double num2;
-	
 	@Test
-	public void testAssertSame() {
-		id = "Hello";
-		id2 = "Hello";
-		assertSame("Objects are not the same.", id, id2);
+	public void testAssertSameString() {
+		String a = "hello";
+		String b = "hello";
+		assertSame("Objects are not the same.", a, b);
 	}
 	
 	@Test
-	public void testAssertNotSame() {
-		int1 = 1;
-		int2 = 2;
-		assertNotSame(int1, int2);
+	public void testAssertSameInt() {
+		int a = 1;
+		int b = 1;
+		assertSame(a, b);
 	}
 	
 	@Test
-	public void testAssertEqualsWithDoubles() {
-		num1 = 10.1234;
-		num2 = 11.0000;
+	public void testAssertNotSameString() {
+		String a = "hello";
+		String b = "Hello";
+		assertNotSame(a, b);
+	}
+	
+	@Test
+	public void testAssertNotSameInt() {
+		int a = 1;
+		int b = 2;
+		assertNotSame(a, b);
+	}
+
+//Assert Equals
+	@Test
+	public void testAssertEqualsStrings() {
+		String a = "hello";
+		String b = "hello";
+		assertEquals(a,b);
+	}
+	
+	@Test
+	public void testAssertEqualsDoubles() {
+		Double a = 10.1234;
+		Double b = 11.0000;
 		Double expectedDelta = 1.85;
-		assertEquals("Delta greater than allowed.", num1, num2, expectedDelta);
+		assertEquals("Delta greater than allowed.", a, b, expectedDelta);
 		//Junit's expected/actual messaging is confusing here: java.lang.AssertionError: Delta greater than allowed. expected:<10.1234> but was:<12.0>
 	}
 	
 	@Test
-	public void testAssertThatIs() {
-		id = "resultStats";
-		assertThat(id, is("resultStats"));
+	public void testAssertNotEquals() {
+		String a = "hello";
+		String b = "Hello";
+		assertNotEquals(a,b);
 	}
+
+//Assert That(T actual, org.hamcrest.Matcher<T> matcher)
+	//Equality
+	@Test
+	public void testAssertThatIsForEquality() {
+		String a = "hello";
+		assertThat(a, is("hello"));
+	}
+	
+	@Test
+	public void testAssertThatIsNotForEquality () {
+		String a = "hello";
+		assertThat(a, is(not("Hello")));
+	}
+	
+	@Test
+	public void testAssertThatIs() {
+		String a = "hello";
+		assertThat(a, is(equalTo("hello")));
+	}
+	
+	@Test
+	public void testAssertThatIsEqualWithMsg() {
+		String a = "hello";
+		String msg = "Compared Objects are not same";
+		assertThat(msg, a, is(equalTo("hello")));
+	}
+	
+	@Test
+	public void testAssertThatIsNotEqual() {
+		String a = "hello";
+		assertThat(a, is(not(equalTo("Hello"))));
+	}
+	
+	
+	@Test
+	public void testEquals() {
+			String aName = new String("emily");
+			String anotherName = new String("emily");
+			System.out.println(aName == anotherName);
+	}
+	
+	@Test
+	public void testEqualTo() {
+		String aName = new String("emily");
+		String anotherName = new String("emily");
+		System.out.println(aName.equals(anotherName));
+	}
+	
+//Assert True
+	
+	
+//Fail
 }
